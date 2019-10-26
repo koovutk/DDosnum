abe204c6aed0664118c24970d16ba9d7ba57ad7d
koovutk <44377490+koovutk@users.noreply.github.com>
2019-10-26 12:55:34 +0700

    Update xerxes.c
    X

--- xerxes.c
+++ xerxes.c
@@ -49,8 +49,8 @@ void broke(int s) {
 	// do nothing
 }
 
-#define CONNECTIONS 8
-#define THREADS 48
+#define CONNECTIONS 40
+#define THREADS 60
 
 void attack(char *host, char *port, int id) {
 	int sockets[CONNECTIONS];
